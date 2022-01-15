29ddad7bda059f19d9cb42b858f1afcf40d145ea
Juan Moises Torrijos <juanmtorrijos@gmail.com>
2022-01-15 14:10:49 -0500

    Adds test folder.

--- __tests__/fibonacci.test.js
+++ __tests__/fibonacci.test.js
@@ -1,4 +1,4 @@
-const fibonacci = require('./fibonacci')
+const fibonacci = require('../fibonacci')
 
 test('Tests if fibonacci sequence is correct', () => {
   expect(fibonacci(1,1)).toStrictEqual([1,1])
